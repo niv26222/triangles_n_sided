c8fbe6be15a73dd8e77b20fa8e5be75d4a6275d0
Niv Ben Abat <niv.benavat@gmail.com>
2019-06-19 15:02:13 +0300

    Add files via upload

---
+++ triangles_n_sided.cpp
@@ -0,0 +1,25 @@
+// C++ program to implement the above problem 
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Function to find the number of triangles 
+void findTriangles(int n) 
+{ 
+    int num; 
+    num = n * (n - 4); 
+  
+    // print the number of triangles 
+    cout << num; 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    // initialize the number of sides of a polygon 
+    int n; 
+    n = 6; 
+  
+    findTriangles(n); 
+  
+    return 0; 
+} 
